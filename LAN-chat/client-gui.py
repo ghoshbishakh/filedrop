e0a0f0d7330f4e0098c0f99cdc7ae6c9c3c8491e
ghoshbishakh <ghoshbishakh@gmail.com>
2014-09-23 21:05:51 +0530

    client GUI layut updated, text view added properly NOW task that remains: to use it with twisted reactor loop

--- LAN-chat/client-gui.py
+++ LAN-chat/client-gui.py
@@ -4,22 +4,35 @@ from gi.repository import Gtk
 
 #GUI ------------
 
+#loading XML from glade file
+GUIbuilder = Gtk.Builder()
+GUIbuilder.add_from_file("client-gui-layout.glade")
 
-class chatWindow(Gtk.Window):
-	def __init__(self):
-		Gtk.Window.__init__(self, title="LAN Chat")
+window = GUIbuilder.get_object("chatWindow")
+chatTextBuffer = GUIbuilder.get_object("chatTextBuffer")
 
-		self.button = Gtk.Button(label="Click Here")
-		self.button.connect("clicked", self.onButtonClick)
-		self.add(self.button)
-	def onButtonClick(self, widget):
-		self.button.label="Clicked"
-		self.button.set_label("Clicked!!")
-		print "Clicked!!"
-		
-win = chatWindow()
-win.connect("delete-event", Gtk.main_quit)
-win.show_all()
+
+chatTextBuffer.set_text("THIS IS A TEST TEXT")
+chatTextBuffer.set_text("THIS IS A TEST TEXT2")
+
+
+class Handler:
+	"""Contains the signal handlers"""
+	def onDeleteWindow(self, *args):
+		Gtk.main_quit(*args)
+
+	def onSendButtonClick(self, *args):
+		sendChat()
+
+def sendChat():
+	endIter= chatTextBuffer.get_end_iter()
+	text = "\nYOYO"
+	chatTextBuffer.insert(endIter, text, length=len(text))
+
+
+GUIbuilder.connect_signals(Handler())
+
+window.show_all()
 Gtk.main()
 
 
